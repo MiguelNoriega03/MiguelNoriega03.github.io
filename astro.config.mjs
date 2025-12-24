3a185af1280be6e7e151beb679bba6bea354bfe9
Miguel Noriega <110911031+MiguelNoriega03@users.noreply.github.com>
2025-12-23 19:16:24 -0600

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: siteConfig.website,
+    site: 'https://MiguelNoriega03.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
