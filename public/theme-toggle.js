47aef1e541a84aa3c6c590e0452c7c83aab8ebc2
MiguelNoriega03 <110911031+MiguelNoriega03@users.noreply.github.com>
2026-01-25 15:16:34 -0600

    Default tema claro

--- public/theme-toggle.js
+++ public/theme-toggle.js
@@ -24,7 +24,7 @@
 
     function getActiveTheme() {
         const stored = getStoredTheme();
-        return stored || getSystemTheme();
+        return stored || 'light';
     }
 
     function applyTheme(theme) {
